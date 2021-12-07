22c85a4a87ef5e7522835fb1e9d8bf9c4e62d1b7
Michael Rider <mikepdxrider@gmail.com>
2021-12-07 14:27:10 -0800

    adds LandingPage test

---
+++ src/views/LandingPage/LandingPage.test.jsx
@@ -0,0 +1,19 @@
+import { screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import LandingPage from "./LandingPage.jsx";
+
+it ('renders PlayerCard', () => {
+    const { container } = render(
+    <MemoryRouter>
+        <LandingPage />
+    </MemoryRouter>
+     )
+    const title = screen.getByText('Welcome to the kickball league!');
+    // or...
+    // const playerName = screen.getByText(/welcome to the kickball league/i);
+    
+
+    expect(title).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+})
