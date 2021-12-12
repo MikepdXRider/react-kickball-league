38d04bebb3256b3cd16cfacc8e532314a877987f
Michael Rider <mikepdxrider@gmail.com>
2021-12-11 17:46:09 -0800

    can't get the testing to pass CI, errors related to 'act()'. Accepting the point hit on this assignment. I don't have the resources or understanding necessary to solve this issue.

--- src/views/Teams/TeamsList.test.jsx
+++ src/views/Teams/TeamsList.test.jsx
@@ -1,10 +1,41 @@
 import { screen } from "@testing-library/react";
 import { render } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 import { MemoryRouter } from "react-router";
 import TeamsList from "./TeamsList.jsx";
 
 //need to mock out the server for this to work again. 
-it.skip('renders TeamsList page', async () => {
+const mockTeamArr = [{
+    id: 6,
+    created_at: '2021-12-08T20:26:24.408898+00:00',
+    name: 'redirect me!',
+    city: 'okay',
+    state: 'go',
+    players: []
+}];
+
+const server = setupServer(
+    rest.get(
+        'https://bmvgpxsbwlwuetqtoczm.supabase.co/rest/v1/teams',
+        (req, res, ctx) => {
+        return res(ctx.json(mockTeamArr));
+    }),
+    rest.post(
+        'https://bmvgpxsbwlwuetqtoczm.supabase.co/rest/v1/teams',
+        (req, res, ctx) => {
+        return res(ctx.json([mockTeamArr]));
+    })
+    );
+
+beforeAll(() => {
+    server.listen();
+});
+
+afterAll(() => {
+    server.close();
+});
+it('renders TeamsList page w/ data from api', async () => {
     const { container } = render(
             <MemoryRouter>
                 <TeamsList />
